eebecbc325a0628055058b17ed7940af75f6a3b4
angusbayley <angusbayley@gmail.com>
2021-08-19 10:14:59 +0100

    initial commit, the original work from 2 years ago

---
+++ conran_shop_filtering.js
@@ -0,0 +1,48 @@
+/* Price filtering for the Conran Shop website
+
+https://www.conranshop.co.uk/outlet.html
+
+
+*/
+
+filter = function(threshold) {
+    /*  threshold: only see items discounted by more than this (%)  */
+
+    var counter = {
+        hidden: 0,
+        unhidden: 0,
+        aboveThreshold: 0,
+        belowThreshold: 0
+    };
+
+    // TODO: extract DOM parsing rules for easier reading/updating...
+
+    var discountLabels = document.getElementsByClassName('discount-label');
+    for (let label of discountLabels) {
+        parent = label.parentElement.parentElement;
+        if (extractNumberFromDiscountText(label.innerText) >= threshold) {
+            counter.aboveThreshold += 1;
+            if (parent.hidden) {
+                parent.hidden = false;
+                counter.unhidden += 1;
+            }
+        }
+        if (extractNumberFromDiscountText(label.innerText) < threshold) {
+            counter.belowThreshold += 1;
+            if (!parent.hidden) {
+                parent.hidden = true;
+                counter.hidden += 1;
+            }
+        }
+    }
+
+    console.log(
+        "set threshold to " + threshold + "%, leaving " + counter.aboveThreshold + " items visible. " + counter.hidden +
+        " items were hidden and " + counter.unhidden + " items were unhidden.");
+}
+
+extractNumberFromDiscountText = function(text) {
+    /*  text will be a string in the form "-50%"  */
+    substring = text.substr(1, text.indexOf("%")-1);
+    return parseInt(substring);
+}
